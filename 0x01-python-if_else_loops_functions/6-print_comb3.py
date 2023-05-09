3a87d4c8679f46a0aa716b5518662dd12a43942a
Winfred <winfrednginakilonzo@gmail.com>
2023-05-09 10:13:00 -0700

    prints combination of two digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+for num_1 in range(0, 9):
+    for num_2 in range((num_1 + 1), 10):
+        print("{:d}{:d}".format(num_1, num_2), end='')
+        if (num_1 < 8 or num_2 < 9):
+            print(", ", end='')
+
+print("\n", end='') 
