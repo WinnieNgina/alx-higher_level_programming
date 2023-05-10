c27a0aec4036608a619ecc626d8b186e9cb77b3b
Winfred <winfrednginakilonzo@gmail.com>
2023-05-09 17:00:58 -0700

    remove character at n position

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def remove_char_at(string, n):
+    if n < 0 or n >= len(string):
+        return string  # Return the original string if n is out of bounds
+
+    new_string = ""  # Initialize an empty string to store the modified string
+
+    for i in range(len(string)):
+        if i != n:
+            new_string += string[i]  # Append the character to the new string if it's not at position n
+
+    return new_string
