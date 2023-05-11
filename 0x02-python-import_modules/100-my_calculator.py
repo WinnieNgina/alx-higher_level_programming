f2c6d75a16d4a9475c0a576bf7245015037b2cf2
Winfred <winfrednginakilonzo@gmail.com>
2023-05-11 12:18:43 -0700

    import exit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ from calculator_1 import add, sub, mul, div
 if __name__ == '__main__':
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
     a = int(sys.argv[1])
     operator = sys.argv[2]
     b = int(sys.argv[3])
@@ -18,5 +18,5 @@ if __name__ == '__main__':
         result = div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        sys.exit(1)
     print("{:d} {:s} = {:d}".format(a, operator, b))
