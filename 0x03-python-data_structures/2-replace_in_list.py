c7348c46ae4218fdf1340c963fc161fa0b420551
Winfred <winfrednginakilonzo@gmail.com>
2023-05-13 02:01:05 -0700

    rectify issues on len

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     x = len(my_list)
-    if idx < 0 or idx > x:
+    if idx < 0 or idx > (x-1):
         return my_list
     else:
         my_list[idx] = element
