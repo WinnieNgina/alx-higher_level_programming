e42babd6c27746bdb52da009bc6cc56936863c9c
Winfred <winfrednginakilonzo@gmail.com>
2023-05-13 03:44:44 -0700

    bugs fixed

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    my_new_string = my_string.translate({ord(i): '' for i in 'cC'})
-    return (my_new_string)
+    new_string = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
