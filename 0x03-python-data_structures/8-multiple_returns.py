ddecb85188be19060f080d14877e4bf71a812fc1
Winfred <winfrednginakilonzo@gmail.com>
2023-05-14 09:48:14 -0700

    add none to return of fitst character

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first = sentence[0]
-    return(length, first)
+    if length > 0:
+        first = sentence[0]
+        return(length, first)
+    else:
+        first = None
+        return(length, first)
