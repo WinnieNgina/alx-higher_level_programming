44c31e8e119f8bd4fe1380bcc5860fbf2ecc0e70
Winfred <winfrednginakilonzo@gmail.com>
2023-06-06 17:34:08 -0700

    text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -2,9 +2,15 @@
 def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    for i in text:
-        if (i == '.' or i == '?' or i == ':'):
-            print(i)
+
+    skip_space = False
+    for char in text:
+        if char == '.' or char == '?' or char == ':':
+            print(char)
             print()
+            skip_space = True
+        elif char == ' ' and skip_space:
+            continue
         else:
-            print(i, end="")
+            print(char, end='')
+            skip_space = False
