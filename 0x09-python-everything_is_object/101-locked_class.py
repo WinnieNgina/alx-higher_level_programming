68d30d47a0f9a7e986a93cfabdb9fba551231271
Winfred <winfrednginakilonzo@gmail.com>
2023-06-07 07:46:46 -0700

    add comments

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+"""locked class module"""
+
+
 class LockedClass:
+    """LockedClass
+
+        attr:
+            __slots__ (private attribute)
+    """
     __slots__ = ['first_name']
