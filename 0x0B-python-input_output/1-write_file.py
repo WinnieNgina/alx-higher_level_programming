cbb6ed2ff09fdbb3fb392f56cd4e370513da1348
Winfred <winfrednginakilonzo@gmail.com>
2023-06-13 05:42:29 -0700

    write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+Write a string to a text file
+"""
+
+
+def write_file(filename="", text=""):
+
+    """
+    Write a string to a text file
+    """
+    with open(filename, 'w', encoding="utf-8") as textfile:
+        return textfile.write(text)
