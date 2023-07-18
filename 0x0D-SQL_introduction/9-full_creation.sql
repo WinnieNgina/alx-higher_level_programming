374342b9b5d05f349d1c1c3c7f8cc4d54c036076
Winfred <winfrednginakilonzo@gmail.com>
2023-07-19 01:09:03 +0300

    Full creation ammendment

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -3,7 +3,7 @@ CREATE TABLE second_table(
 id INT,
 name VARCHAR(256),
 score INT);
-INSERT INTO table_name (id, name, score)
+INSERT INTO second_table (id, name, score)
 VALUES
 	(1, 'John', 10),
 	(2, 'Alex' 10),
