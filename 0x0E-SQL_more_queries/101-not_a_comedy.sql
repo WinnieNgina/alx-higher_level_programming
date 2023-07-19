238636701c66a4507fe5c3c7ccdf56cc6860e1a7
Winfred <winfrednginakilonzo@gmail.com>
2023-07-20 01:45:38 +0300

    remove one select statement

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,13 +1,9 @@
 -- lists all shows without the genre Comedy
-SELECT title
+
+SELECT tv_shows.title
 FROM tv_shows
-WHERE id NOT IN (
-  SELECT show_id
-  FROM tv_show_genres
-  WHERE genre_id = (
-    SELECT id
-    FROM tv_genres
-    WHERE name = 'Comedy'
-  )
-)
-ORDER BY title ASC;
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name != 'Comedy' OR tv_genres.name IS NULL
+ORDER BY tv_shows.title ASC;
+
