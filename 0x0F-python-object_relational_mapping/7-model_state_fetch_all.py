52c3b5603221da262ff75417c4b45c83cfed50fe
Winfred <winfrednginakilonzo@gmail.com>
2023-08-19 00:55:12 +0300

    lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
+
+if __name__ == "__main__":
+    user = argv[1]
+    pas = argv[2]
+    db = argv[3]
+    dbUrl = 'mysql+mysqldb://{}:{}@localhost/{}'.format(user, pas, db)
+    engine = create_engine(dbUrl)
+    Session = sessionmaker(bind = engine) #returns a function
+    session = Session()
+
+    for instance in session.query(State).order_by(State.id):
+        print("{}: {}".format(instance.id, instance.name))
