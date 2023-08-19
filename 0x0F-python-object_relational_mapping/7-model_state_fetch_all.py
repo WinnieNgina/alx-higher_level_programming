1c635a8074ef97a2f8930b520c3e6dbe3bcc6924
Winfred <winfrednginakilonzo@gmail.com>
2023-08-19 05:00:32 -0700

    pool_pre_ping=True argument has been moved outside of the .format()

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-'''
-Retrieves lists all State objects from the database hbtn_0e_6_us
-'''
 
+"""
+Retrieves lists all State objects from the database hbtn_0e_6_us
+"""
+from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sys import argv
 from model_state import Base, State
 
 if __name__ == "__main__":
     user = argv[1]
     pas = argv[2]
     db = argv[3]
+
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(user, pas, db), pool_pre_ping=True
+        .format(user, pas, db),
+        pool_pre_ping=True
     )
-    Session = sessionmaker(bind=engine)
-    """
-    session maker returns a function
-    """
-    session = Session()
 
-    for insitance in session.query(State).order_by(State.id):
-        print("{}: {}".format(inistance.id, inistance.name))
+    Current_Session = sessionmaker(bind=engine)
+    #  session maker returns a function
+    session = Current_Session()
+
+    for instance in session.query(State).order_by(State.id):
+        print("{}: {}".format(instance.id, instance.name))
 
     session.close()
