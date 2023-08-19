25f4d422f25c2abb3f8c4dd18bb2a0ea7f8a69df
Winfred <winfrednginakilonzo@gmail.com>
2023-08-19 12:13:47 +0300

    Use foreign key to retrieve states name on cities table

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''lists all cities from the database hbtn_0e_0_usa'''
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+            host='localhost',
+            user=argv[1],
+            password=argv[2],
+            db=argv[3],
+            port=3306
+            )
+    cur = conn.cursor()
+    query = """
+	SELECT cities.id, cities.name, states.name
+	FROM cities
+	JOIN states ON cities.state_id = states.id
+	ORDER BY id ASC
+	"""
+    cur.execute(query)
+    cities_list = cur.fetchall()
+    for city in cities_list:
+        print(city)
+    cur.close()
+    conn.close()
