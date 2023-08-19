32a4bae976033b40786bf6a121c602d9bddec95f
Winfred <winfrednginakilonzo@gmail.com>
2023-08-19 02:38:36 -0700

    lists all cities of that state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''lists all cities from the database hbtn_0e_0_usa'''
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    state_name = argv[4]
+    conn = MySQLdb.connect(
+            host='localhost',
+            user=argv[1],
+            password=argv[2],
+            db=argv[3],
+            port=3306
+            )
+    cur = conn.cursor()
+    query = """
+    SELECTcities.name
+    FROM cities
+    JOIN states ON cities.state_id = states.id
+    WHERE states.name = %s
+    ORDER BY id ASC
+    """
+    # cities.state_id is a foreign key in a cities table
+    cur.execute(query, (state_name,))
+    cities_list = cur.fetchall()
+    for city in cities_list:
+        print(city)
+    cur.close()
+    conn.close()
