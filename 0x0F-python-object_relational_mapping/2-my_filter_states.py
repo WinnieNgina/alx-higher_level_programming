16238163a23acd9233a6efe250429846b21cd2cd
Winfred <winfrednginakilonzo@gmail.com>
2023-08-19 00:48:28 -0700

    Retrieve data for a specific state

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''lists all states from the database hbtn_0e_0_usa'''
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+            host='localhost',
+            user=argv[1],
+            password=argv[2],
+            db=argv[3],
+            port=3306
+            )
+    cur = conn.cursor()
+    query = "SELECT * FROM states WHERE name = argv[4] ORDER BY id ASC"
+    cur.execute(query)
+    states_list = cur.fetchall()
+    for state in states_list:
+        print(state)
+    cur.close()
+    conn.close()
