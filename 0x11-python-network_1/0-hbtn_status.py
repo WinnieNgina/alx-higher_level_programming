85e8a7db2b5040291204ec85177d51f23855352d
Winfred <winfrednginakilonzo@gmail.com>
2023-09-01 16:39:13 +0300

    Fix pycodestyle

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,8 +5,8 @@ import urllib.request
 Module to fetch specified link
 '''
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        content = response.read()
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
         decode_content = content.decode('utf-8')
         print("Body response:")
         print(f"\t- type: {type(content)}")
