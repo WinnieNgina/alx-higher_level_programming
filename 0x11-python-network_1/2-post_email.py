50757b5fd7e99a0be09221fc62ae20170024a8cc
Winfred <winfrednginakilonzo@gmail.com>
2023-09-02 01:03:30 +0300

    POST an email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+sends a request to the URL
+Displays the value of the X-Request-Id
+'''
+import urllib.request
+import urllib.parse 
+import sys
+if __name__ == "__main__":
+    url = sys.argv[1]  
+params = {    
+    "email": sys.argv[1]     
+}        
+query_string = urllib.parse.urlencode( params )    
+data = query_string.encode( "ascii" )    
+    
+with urllib.request.urlopen( url, data ) as response:     
+    response_text = response.read().decode("utf-8")     
+    print( response_text ) 
