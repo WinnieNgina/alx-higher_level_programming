4268922b40398287d8794d8947e69df3d904edd8
Winfred <winfrednginakilonzo@gmail.com>
2023-09-01 16:28:43 -0700

    use format for strings

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,11 +5,12 @@ Displays the value of the X-Request-Id
 '''
 import urllib.request
 import sys
+
 if __name__ == "__main__":
     url = sys.argv[1]
     try:
         with urllib.request.urlopen(url) as response:
             encode_response = response.read().decode('utf-8')
-            print(encode_response)
+            print("{}".format(encode_response))
     except urllib.error.HTTPError as e:
         print("Enter code: {}".format(e.code))
