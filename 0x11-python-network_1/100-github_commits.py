b64025dc2ec0976757578c502f002342c507a958
Winfred <winfrednginakilonzo@gmail.com>
2023-09-03 16:08:30 -0700

    Fix errors

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,15 +5,17 @@ uses the GitHub API to retrieve the first 10 commits
 
 import sys
 import requests
-from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
-    repo_name = sys.argv[1]
-    user_name = sys.argv[2] 
-    response = requests.get('https://developer.github.com/v3/repos/user_name/repo_name/commits/')
-    commits_data = response.json()
-    for commit in commits_data[:10]:  # Get the first 10 commits
-        sha = commit['sha']
-        author_name = commit['commit']['author']['name']
-        print(f"{}: {}".format(sha, author_name))
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+            sys.argv[2], sys.argv[1])
+    response = requests.get(url)
+    commits = response.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
