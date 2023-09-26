c88e47eca744f1d587f69a684e8141c0606742b1
Winfred <winfrednginakilonzo@gmail.com>
2023-09-26 18:15:09 +0300

    Make a get request and print status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const request = require('request');
+const args = process.argv;
+const url = args[2];
+request.get(url, (error, response) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('Code: ', response && response.statusCode);
+  }
+});
