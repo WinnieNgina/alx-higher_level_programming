41c1829e171122a5d136e3db1dacb3e360175a36
Winfred <winfrednginakilonzo@gmail.com>
2023-09-26 18:42:14 +0300

    Fix message

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,6 +6,6 @@ request(url, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
-    console.log('Code:', response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });
