4a460583ca1aa021702c04ced9ff8db8f4e5ec6d
Winfred <winfrednginakilonzo@gmail.com>
2023-09-27 04:18:09 +0300

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,27 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
+
 const url = process.argv[2];
 
 request(url, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
-    const data = JSON.parse(body);
-    for (const characterUrl of data.characters) {
-      request(characterUrl, (error, response, body) => {
-        if (error) {
-          console.error(error);
-        } else {
-          const characterData = JSON.parse(body);
-          let hasWedge = 0;
-          for (const key in characterData) {
-            if (characterData[key].name === 'Wedge Antilles') {
-              hasWedge += 1;
-            }
-          }
-          console.log(hasWedge);
-        }
-      });
-    }
+    const films = JSON.parse(body).results;
+    const wedgeAntillesMovies = films.filter(film =>
+      film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
+    );
+    console.log(wedgeAntillesMovies.length);
   }
 });
