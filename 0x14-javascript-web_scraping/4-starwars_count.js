de97232844167d5d9e43290cd6b3573f77a146a1
Winfred <winfrednginakilonzo@gmail.com>
2023-09-27 04:31:03 +0300

    Use id to filter

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -8,9 +8,11 @@ request(url, (error, response, body) => {
     console.error(error);
   } else {
     const films = JSON.parse(body).results;
-    const wedgeAntillesMovies = films.filter(film =>
-      film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
-    );
-    console.log(wedgeAntillesMovies.length);
+    const moviesWithWedge = films.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1 // if found
+        : count; // if not found
+    }, 0); // initiliazing count to zero
+    console.log(moviesWithWedge);
   }
 });
